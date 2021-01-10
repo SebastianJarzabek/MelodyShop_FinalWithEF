9eaff5bf466483298f58cb51a8af5af8a5e05f14
s.jarzabek <sebastian.jarzabek@outlook.com>
2021-01-10 12:49:55 +0100

    fix

--- MelodyShop/Controllers/CartsController.cs
+++ MelodyShop/Controllers/CartsController.cs
@@ -1,137 +1,133 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using MelodyShop.DAL;
 using MelodyShop.Models;
 
 namespace MelodyShop.Controllers
 {
-    public class CartsController : Controller
+  public class CartsController : Controller
+  {
+    private ApplicationDbContext db = new ApplicationDbContext();
+
+    // GET: Carts
+    public ActionResult Index()
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
-
-        // GET: Carts
-        public ActionResult Index()
-        {
-            var carts = db.Carts.Include(c => c.Product).Include(c => c.User);
-            return View(carts.ToList());
-        }
-
-        // GET: Carts/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Cart cart = db.Carts.Find(id);
-            if (cart == null)
-            {
-                return HttpNotFound();
-            }
-            return View(cart);
-        }
-
-        // GET: Carts/Create
-        public ActionResult Create()
-        {
-            ViewBag.productId = new SelectList(db.Products, "id", "name");
-            ViewBag.userId = new SelectList(db.Users, "id", "modyfity");
-            return View();
-        }
-
-        // POST: Carts/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,userId,productId,quantity,modyfity,modyfityDate,created,createdDate")] Cart cart)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Carts.Add(cart);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.productId = new SelectList(db.Products, "id", "name", cart.productId);
-            ViewBag.userId = new SelectList(db.Users, "id", "modyfity", cart.userId);
-            return View(cart);
-        }
-
-        // GET: Carts/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Cart cart = db.Carts.Find(id);
-            if (cart == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.productId = new SelectList(db.Products, "id", "name", cart.productId);
-            ViewBag.userId = new SelectList(db.Users, "id", "modyfity", cart.userId);
-            return View(cart);
-        }
-
-        // POST: Carts/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,userId,productId,quantity,modyfity,modyfityDate,created,createdDate")] Cart cart)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(cart).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.productId = new SelectList(db.Products, "id", "name", cart.productId);
-            ViewBag.userId = new SelectList(db.Users, "id", "modyfity", cart.userId);
-            return View(cart);
-        }
-
-        // GET: Carts/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Cart cart = db.Carts.Find(id);
-            if (cart == null)
-            {
-                return HttpNotFound();
-            }
-            return View(cart);
-        }
-
-        // POST: Carts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Cart cart = db.Carts.Find(id);
-            db.Carts.Remove(cart);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+      var carts = db.Carts.Include(c => c.Product).Include(c => c.User);
+      return View(carts.ToList());
     }
+
+    // GET: Carts/Details/5
+    public ActionResult Details(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Cart cart = db.Carts.Find(id);
+      if (cart == null)
+      {
+        return HttpNotFound();
+      }
+      return View(cart);
+    }
+
+    // GET: Carts/Create
+    public ActionResult Create()
+    {
+      ViewBag.productId = new SelectList(db.Products, "id", "name");
+      ViewBag.userId = new SelectList(db.Users, "id", "modyfity");
+      return View();
+    }
+
+    // POST: Carts/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create([Bind(Include = "id,userId,productId,quantity,modyfity,modyfityDate,created,createdDate")] Cart cart)
+    {
+      if (ModelState.IsValid)
+      {
+        db.Carts.Add(cart);
+        db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+
+      ViewBag.productId = new SelectList(db.Products, "id", "name", cart.productId);
+      ViewBag.userId = new SelectList(db.Users, "id", "modyfity", cart.userId);
+      return View(cart);
+    }
+
+    // GET: Carts/Edit/5
+    public ActionResult Edit(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Cart cart = db.Carts.Find(id);
+      if (cart == null)
+      {
+        return HttpNotFound();
+      }
+      ViewBag.productId = new SelectList(db.Products, "id", "name", cart.productId);
+      ViewBag.userId = new SelectList(db.Users, "id", "modyfity", cart.userId);
+      return View(cart);
+    }
+
+    // POST: Carts/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit([Bind(Include = "id,userId,productId,quantity,modyfity,modyfityDate,created,createdDate")] Cart cart)
+    {
+      if (ModelState.IsValid)
+      {
+        db.Entry(cart).State = EntityState.Modified;
+        db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+      ViewBag.productId = new SelectList(db.Products, "id", "name", cart.productId);
+      ViewBag.userId = new SelectList(db.Users, "id", "modyfity", cart.userId);
+      return View(cart);
+    }
+
+    // GET: Carts/Delete/5
+    public ActionResult Delete(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Cart cart = db.Carts.Find(id);
+      if (cart == null)
+      {
+        return HttpNotFound();
+      }
+      return View(cart);
+    }
+
+    // POST: Carts/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Cart cart = db.Carts.Find(id);
+      db.Carts.Remove(cart);
+      db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        db.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+  }
 }
